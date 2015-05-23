5462b36ca1892c1a886e375b09ca473d9e547565
Ryan <hiphubryan@gmail.com>
2015-05-23 16:09:59 +0800

    Finish class 17

--- Autolayout/User.swift
+++ Autolayout/User.swift
@@ -15,6 +15,8 @@ struct User{
   let login: String
   let password: String
   
+  var lastLogin = NSDate.demoRandom()
+  
   static func login(login: String, password: String) -> User? {
     if let user = database[login] {
       if user.password == password {
@@ -28,11 +30,11 @@ struct User{
   static let database: Dictionary<String, User> = {
     var theDatabase = Dictionary<String, User>()
     for user in [
-      User(name: "John Appleseed", company: "Apple", login: "japple", password: "foo"),
-      User(name: "Madison Bumgarner", company: "World Champion San Francisco Giants", login: "madbum", password: "foo"),
+      User(name: "John Appleseed", company: "Apple", login: "japple", password: "foo", lastLogin: NSDate.demoRandom()),
+      User(name: "Madison Bumgarner", company: "World Champion San Francisco Giants", login: "madbum", password: "foo", lastLogin: NSDate.demoRandom()),
       
-      User(name: "John Hennessy", company: "Stanford", login: "hennessy", password: "foo"),
-      User(name: "Bad Guy", company: "Criminals, Inc.", login: "baddie", password: "foo")
+      User(name: "John Hennessy", company: "Stanford", login: "hennessy", password: "foo", lastLogin: NSDate.demoRandom()),
+      User(name: "Bad Guy", company: "Criminals, Inc.", login: "baddie", password: "foo", lastLogin: NSDate.demoRandom())
       ] {
         theDatabase[user.login] = user
     }
@@ -40,4 +42,12 @@ struct User{
   }()
   
   
+}
+
+
+private extension NSDate {
+  class func demoRandom() -> NSDate {
+    let randomIntervalIntoThePast = -Double(arc4random() % 60*60*24*20)
+    return NSDate(timeIntervalSinceNow: randomIntervalIntoThePast)
+  }
 }
--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var imageView: UIImageView!
   
+  @IBOutlet weak var lastLoginLabel: UILabel!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     updateUI()
@@ -29,17 +32,63 @@ class ViewController: UIViewController {
   
   private func updateUI() {
     passwordField.secureTextEntry = secure
-    passwordLabel.text = secure ? "Secure Password" : "Password"
+    
+    let password = NSLocalizedString("Password", comment: "Prompt for the user's password when it is not secure (i.e. plain text)")
+    let securedPassword = NSLocalizedString("Secured Password", comment: "Prompt for an obscured (not plain text) password")
+    
+    passwordLabel.text = secure ? securedPassword : password
     nameLabel.text = loggedInUser?.name
     companyLabel.text = loggedInUser?.company
     image = loggedInUser?.image
+    
+    if let lastLogin = loggedInUser?.lastLogin {
+      
+      let dateFormatter = NSDateFormatter()
+      dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+      dateFormatter.dateStyle = NSDateFormatterStyle.NoStyle
+      let time = dateFormatter.stringFromDate(lastLogin)
+      let numberFormatter = NSNumberFormatter()
+      
+      numberFormatter.maximumFractionDigits = 1
+      
+      let daysAgo = numberFormatter.stringFromNumber(-lastLogin.timeIntervalSinceNow/(60*60*24))!
+      
+      let lastLoginFormatString = NSLocalizedString("Last Login %@ days ago at %@", comment: "Reports the number of days ago and time that the user last logged in")
+      
+      lastLoginLabel.text = String.localizedStringWithFormat(lastLoginFormatString, daysAgo, time)
+    
+    } else {
+      lastLoginLabel.text = ""
+    }
+    
   }
 
   @IBAction func toggleSecurity() {
     secure = !secure
   }
+  
+  
+  
+  private struct AlertStrings {
+    struct LoginError {
+      static let Title = NSLocalizedString("Login Error", comment: "Title of alert when user types in an incorrect user name or password")
+      
+      static let Message = NSLocalizedString("Invalid user name or password", comment: "Message in an alert when the user types in an incorrect user name or password")
+      
+      static let DismissButton = NSLocalizedString("Try Again", comment: "The only button available in an alert presented when the user types incorrect user name or password")
+    }
+  }
+  
   @IBAction func login() {
     loggedInUser = User.login(loginField.text ?? "" , password: passwordField.text ?? "")
+    
+    if loggedInUser == nil {
+      let alert = UIAlertController(title: AlertStrings.LoginError.Title, message: AlertStrings.LoginError.Message, preferredStyle: UIAlertControllerStyle.Alert)
+      
+      alert.addAction(UIAlertAction(title: AlertStrings.LoginError.DismissButton, style: UIAlertActionStyle.Default, handler: nil))
+      
+      presentViewController(alert, animated: true, completion: nil)
+    }
   }
   
   
